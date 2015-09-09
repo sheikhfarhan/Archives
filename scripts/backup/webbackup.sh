c9bcabf4b03f67497c49cf5a6a2e99e47740b373
Sheikh Farhan <sheikhfarhan@users.noreply.github.com>
2015-09-09 12:00:07 +0800

    Update webbackup.sh

--- scripts/backup/webbackup.sh
+++ scripts/backup/webbackup.sh
@@ -7,7 +7,7 @@
 ## Place this file in /home/path/under/user
 ## automatic daily / weekly / monthly backup to S3.
 ## Add the line below to cronjob (user not root!) for a 4am daily run
-## 0 4 * * * sh /home/gandalf/webbackup.sh auto
+## 0 4 * * * sh webbackup.sh auto
 
 
 
